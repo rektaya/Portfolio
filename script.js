4a4acb92d763e1ca7d356409d872fed5d11d29db
rektaya <rektaya@gmail.com>
2023-06-30 13:27:08 +0200

    images and burger menu

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    const burger = document.getElementById('hamburger');
-    const nav = document.getElementById('nav');
-    console.log(burger);
-    let menu = 'closed';
+    console.log("hello")
+    const burger = document.getElementById('burger');
+    const nav = document.querySelector('.side-menu');
+    console.log(nav)
+    let menuClosed = true;
     burger.addEventListener("click", function(event) {
-        if (menu == 'closed') {
-            menu = 'opened';
-            nav.classList.remove('menu-closed');
-            nav.classList.add('menu-opened');
-        } else {
-            menu = 'closed';
-            nav.classList.remove('menu-opened');
-            nav.classList.add('menu-closed');
+        if (menuClosed) {
+            menuClosed = false;
+            nav.classList.remove('hidden');
+            document.body.classList.add('no-scroll');
         }
     })
 
-
-    const readMe = document.getElementById("target");
-    const hiddenP = document.querySelector(".hidden");
-    console.log(readMe);
-    console.log(hiddenP);
-
-    readMe.addEventListener("click", function(event) {
-        console.log('clicked');
-        hiddenP.classList.remove("hidden");
-        readMe.classList.add("hidden");
-        event.preventDefault();
+    nav.addEventListener("click", function(event) {
+        if(!menuClosed){
+            menuClosed = true;
+            nav.classList.add('hidden');
+            document.body.classList.remove('no-scroll');            
+        }
     })
-}
+})
